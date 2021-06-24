aea263a8cfe73b7362984989cf0d7891115c7e3a
rjay1103 <robertllorente12@gmail.com>
2021-06-24 14:11:07 +0800

    my sample
    mvc sample

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.ViewModel;
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AccountViewModel avm)
+        {
+            if (avm.account.Username.Equals("rj") && avm.account.Password.Equals("123"))
+
+            {
+                Session["username"] = avm.account.Username;
+                return View("Welcome");
+            }
+            else
+            {
+               
+            }
+            ViewBag.Error = "Invalid Account";
+            return View("Index");
+        }
+            public ActionResult Logout()
+        {
+            Session.Remove("username");
+            return RedirectToAction("Index");
+        }
+    }
+}
